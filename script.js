26f80aa285a61dd854a1e6efd3222af8d090aa83
Hubertvariant <hubert.ramos@escola.pr.gov.br>
2025-09-11 23:42:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-function deslizar(){
-    let menu = window.document.querySelector("nav#menu")
-    let borda = window.document.querySelector("div#borda")
-    let i = window.document.querySelector("p#icon")
-    if (borda.style.left === "250px"){
-        i.removeAttribute("class")
-        menu.style.width = "0"
-        borda.style.left = "0"
-        borda.className = "fa-solid fa-arrow-right"
-    }else{
-        i.removeAttribute("class")
-        menu.style.width = "250px"
-        borda.style.left = "250px"
-        borda.className = "fa-solid fa-arrow-left"
+function pefil() {
+    const menu = document.querySelector("div#menu");
+    const currentWidth = window.getComputedStyle(menu).width;
+
+    if (currentWidth === "0px") {
+        menu.style.width = "250px";
+    } else {
+        menu.style.width = "0px";
     }
 }
