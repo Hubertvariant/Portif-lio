1a72ba3c37e06d0bcd08f35b7ed2093452b274e7
Hubertvariant <hubert.ramos@escola.pr.gov.br>
2025-09-12 11:40:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function pefil() {
-    const menu = document.querySelector("div#menu");
-    const currentWidth = window.getComputedStyle(menu).width;
+    let menu = document.querySelector("div#menu")
+    let chat = document.querySelector("div.chat")
+    const currentWidth = window.getComputedStyle(menu).width
 
     if (currentWidth === "0px") {
-        menu.style.width = "250px";
+        menu.style.width = "250px"
+        chat.style.left = "290px"
+        chat.style.width = "calc(100% - 290px)"
     } else {
-        menu.style.width = "0px";
+        menu.style.width = "0px"
+        chat.style.left = "40px"
+        chat.style.width = "calc(100% - 40px)"
     }
 }
